fa9713433e25547a6540264447237374d2c3f7ce
Nata Souza Santos <natasouza.santos50@gmail.com>
2021-05-10 19:16:57 -0300

    first step in upload resized image after s3 trigger

--- index.js
+++ index.js
@@ -1,8 +1,62 @@
+const aws = require("aws-sdk");
+
+const s3 = new aws.S3({ apiVersion: "2006-03-01" });
+
+async function getContentFile(bucket, key) {
+  const params = {
+    Bucket: bucket,
+    Key: key,
+  };
+
+  try {
+    const { ContentType } = await s3.getObject(params).promise();
+    console.log("CONTENT TYPE:", ContentType);
+    return ContentType;
+  } catch (err) {
+    console.log(err);
+    const message = `Error getting object ${key} from bucket ${bucket}. Make sure they exist and your bucket is in the same region as this function.`;
+    console.log(message);
+    throw new Error(message);
+  }
+}
+
 exports.handler = async (event) => {
   // TODO implement
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify("Hello world lamba CD"),
+
+  const bucket = event.Records[0].s3.bucket.name;
+  const key = decodeURIComponent(
+    event.Records[0].s3.object.key.replace(/\+/g, " ")
+  );
+
+  const imageUploaded = await getContentFile(bucket, key);
+
+  const params = {
+    Bucket: bucket,
+    Key: `resized/${key}`,
+    Body: imageUploaded,
   };
-  return response;
+
+  try {
+    await s3.upload(params).promise();
+
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify({ message: "Resize problem" }),
+    };
+
+    return response;
+  } catch (err) {
+    const response = {
+      statusCode: 500,
+      body: JSON.stringify({ message: "Resize problem" }),
+    };
+
+    return response;
+  }
+
+  // const response = {
+  //   statusCode: 200,
+  //   body: JSON.stringify("Hello world lamba CD"),
+  // };
+  // return response;
 };
